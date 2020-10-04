327e5c3c33af13ad2d2d63aa96648aa3db16fee8
Theos <jeremi2811@wp.pl>
2020-10-04 12:36:10 +0200

    Menu

---
+++ Assets/Scripts/PlayButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour
+{
+    Vector3 scale = new Vector3();
+
+    private void Start()
+    {
+        scale = transform.localScale;
+    }
+
+    private void OnMouseDown()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    private void OnMouseEnter()
+    {
+        transform.localScale = new Vector3(transform.localScale.x * 1.3f, transform.localScale.y * 1.3f, transform.localScale.z);
+    }
+
+
+    private void OnMouseExit()
+    {
+        transform.localScale = scale;
+    }
+}
